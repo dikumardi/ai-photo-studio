e2307dfc1ca73a999d895ab88e3e02932f63b561
Diku Mardi <dikumardi047@gmail.com>
2026-04-07 21:30:44 +0530

    refactor: improve code maintainability by removing repeated image handling logic

--- src/controller/upload.controller.js
+++ src/controller/upload.controller.js
@@ -3,6 +3,8 @@ const imageModel = require("../models/image.model");
 const uploadFile = require("../services/storage.service");
 const removeBackground = require("../services/removeBg.service");
 const replaceBackground = require("../services/replaceBg.service");
+const processAndSaveImage = require("../services/imageProcessor.service");
+const changeOutfit = require("../services/outfitChange.service");
 
 exports.uploadImageController = async (req, res) => {
   try {
@@ -19,25 +21,20 @@ exports.uploadImageController = async (req, res) => {
     if (!allowedTypes.includes(file.mimetype)) {
       return res.status(400).json({ message: "Only PNG/JPG allowed" });
     }
-    // 🔥 STEP 1: Remove background
+    //  STEP 1: Remove background
     const bgRemovedImage = await removeBackground(file.buffer);
 
-    // 🔥 STEP 2: Upload to ImageKit
-    const result = await uploadFile(
-      bgRemovedImage.toString("base64"),
-      `${uuidv4()}-${file.originalname}`,
-    );
-
-    // 🔥 STEP 3: Save in DB
-    const image = await imageModel.create({
-      imageUrl: result.url,
+    const image =await processAndSaveImage(
+     bgRemovedImage,
+     file,
       imageName,
-    });
-
-    res.status(201).json({
+      "remove-bg"
+    )
+     res.status(201).json({
       message: "Background removed successfully",
       image,
     });
+   
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: err.message });
@@ -65,20 +62,17 @@ exports.replaceBgController = async (req, res) => {
       return res.status(400).json({ message: "Only images allowed" });
     }
 
-    //  STEP 1: Replace background using Clipdrop
+    //   Replace background using Clipdrop
     const replacedImage = await replaceBackground(file.buffer, prompt);
 
-    //  STEP 2: Upload to ImageKit
-    const result = await uploadFile(
-      replacedImage.toString("base64"),
-      `${uuidv4()}-${file.originalname}`,
-    );
-
-    //  STEP 3: Save in DB
-    const image = await imageModel.create({
-      imageUrl: result.url,
+    const image = await processAndSaveImage(
+      replacedImage,
+      file,
       imageName,
-    });
+      "replace-bg",
+      prompt
+    )
+  
 
     //  Response
     res.status(201).json({
@@ -86,12 +80,8 @@ exports.replaceBgController = async (req, res) => {
       image,
     });
   } catch (err) {
-    console.error("Replace BG Error:", err.message);
-
-    if (err.code === 11000) {
-      return res.status(400).json({ message: "Image name already exists" });
-    }
-
+  
     res.status(500).json({ message: err.message });
   }
 };
+
---
+++ src/services/imageProcessor.service.js
@@ -0,0 +1,27 @@
+const imageModel = require("../models/image.model");
+const uploadFile = require("./storage.service");
+const { v4: uuidv4 } = require("uuid");
+
+
+
+
+async function processAndSaveImage(buffer,file, imageName, operationType, prompt = null) {
+ // Upload to ImageKit
+    const result = await uploadFile(
+      buffer.toString("base64"),
+      `${uuidv4()}-${file.originalname}`,
+    );
+
+    //  Save in DB
+    const image = await imageModel.create({
+      imageUrl: result.url,
+      imageName,
+    operationType,
+    prompt
+    });
+
+    return image
+    
+}
+
+module.exports = processAndSaveImage;
